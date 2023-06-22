beeee523a91749bde2de07551c86b961cf501c85
Shota <eg.shota@yandex.ru>
2023-06-22 13:58:12 +0300

    Forest scene created
    *characters added
    *characters prefabs created
    *forest environment created
    *forest environment prefab created
    *tilemaps added
    **sprite collection updated
    
    Next:
    * audio (bgm, goblin)
    * animation script
    * buttons

---
+++ TechDesignTestProject_Shota/Assets/Scripts/background_parallax.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class background_parallax : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private RawImage _image;
+    private float _imagePositionX;
+
+    void Start()
+    {
+        _image = GetComponent<RawImage>();
+    }
+        
+    void Update()
+    {
+        _imagePositionX += _speed * Time.deltaTime;
+
+        if( _imagePositionX > 1 )
+            _imagePositionX = 0;
+        _image.uvRect = new Rect(_imagePositionX, 0, _image.uvRect.width, _image.uvRect.height);
+    }
+}
